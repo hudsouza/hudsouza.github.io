fdbaa6cc5f8fa924ef5551ec042d2c8ce33a19e9
hudsonmacedo <hudsonmacedo14@gmail.com>
2016-10-15 15:02:41 -0300

    upload site

---
+++ quando-voce-sai/index.php
@@ -0,0 +1,50 @@
+<?php
+$to = "hi@hudsonsouza.com";
+$too = "oi@hudsonsouza.com";
+$subject = "Ela leu um texto";
+$txt = "Quando voce sai";
+$headers = "From: pires.sha@gmail.com.com";
+
+mail($to,$subject,$txt,$headers);
+mail($too,$subject,$txt,$headers);
+?>
+
+<!doctype html>
+<html>
+<head>
+	<meta content="text/html; charset=utf-8" http-equiv="Content-Type" />
+	<link href="https://fonts.googleapis.com/css?family=Merriweather" rel="stylesheet" type="text/css" />
+	<meta content="width=device-width, initial-scale=1" name="viewport" />
+	<title>Quando voc&ecirc; sai</title>
+	<style type="text/css">body {background: #F9F6F4; font-family:'Merriweather', serif; font-size:16px; color: #2F2F2F;}
+div {width: 96%; max-width:660px; padding: 2%; line-height: 28px; text-align:left; margin: auto;}
+p {margin: 3%;}
+hr {border: none; width: 50%; height: 1px; background: #767676; margin: auto;}
+	</style>
+</head>
+<body>
+<div>
+<hr />
+<p>Quando voc&ecirc; sai, me da vontade de correr pra falar tudo que eu sinto e tudo que eu penso sobre voc&ecirc;, sobre mim, sobre n&oacute;s dois.<br />
+<br />
+O motivo n&atilde;o &eacute; por n&atilde;o querer contar isso tudo quando voc&ecirc; est&aacute; comigo, porque eu realmente quero verbalizar cada pequena sensa&ccedil;&atilde;o de felicidade que surge quando estamos juntos, mas, se fizesse isso, nunca pararia de falar.<br />
+<br />
+Ent&atilde;o, eu s&oacute; aproveito em sil&ecirc;ncio e demonstro o m&aacute;ximo que consigo com meu sorriso. Um sorriso t&atilde;o genu&iacute;no que s&oacute; voc&ecirc; consegue despertar. T&atilde;o &uacute;nico que s&oacute; voc&ecirc; consegue ver. T&atilde;o especial que &eacute; s&oacute; seu. Pode parecer que ele acontece mais vezes por outros motivos, mas s&atilde;o diferentes. O &uacute;nico prop&oacute;sito desse sorriso &eacute; condensar minha alegria, gratid&atilde;o, euforia, meus desejos e tantas coisas, que mal sei reconhecer, num gesto puro que te sempre te conquista.<br />
+<br />
+Sinto uma urg&ecirc;ncia de compartilhar tudo com voc&ecirc; no exato momento da nossa despedida. Tem tanto a ser dito, naquele tempo t&atilde;o breve, que adio esse momento para o futuro porque &eacute; imposs&iacute;vel comunicar essa infinidade de emo&ccedil;&otilde;es e reflex&otilde;es no intervalo entre um beijo na janela do carro e a sua acelerada inicial pra casa. Assim que fa&ccedil;o isso, j&aacute; me programo pra falar quando te encontrar da pr&oacute;xima vez, e quando chega essa pr&oacute;xima vez, aquele sorriso toma conta de novo. Tudo &eacute; sentido mas nada &eacute; pronunciado.<br />
+<br />
+O que me permito pensar, sempre, &eacute; que temos tempo. Mas n&atilde;o &eacute; verdade. Ao contr&aacute;rio da maioria do que &eacute; escrito, quando estamos juntos n&atilde;o &eacute; como se o tempo parasse, na verdade, &eacute; o oposto: o tempo acelera quando estou com voc&ecirc;. Imagino que seja o universo fazendo um balanceamento natural pra manter a pr&oacute;pria estabilidade porque essa tens&atilde;o que existe entre apenas duas pessoas n&atilde;o pode ser mantida por muito tempo, ou aconteceria algum colapso sem precedentes. Logo, s&oacute; conseguimos manter a tens&atilde;o e a vontade por esse per&iacute;odo tempo que n&atilde;o &eacute; longo o bastante e que causa um sentimento de perda t&atilde;o grande quando acaba, mas que &eacute; t&atilde;o m&aacute;gico justamente por ser t&atilde;o intenso nesses segundos que ficamos entre ceder ou n&atilde;o.<br />
+<br />
+Apenas saiba que independente da decis&atilde;o que voc&ecirc; toma em seguida, nada me tira o que aconteceu. Se paramos, vai ser uma fra&ccedil;&atilde;o da minha vida que nunca ser&aacute; esquecida, e saiba tamb&eacute;m que a mem&oacute;ria que fica n&atilde;o &eacute; nada al&eacute;m de felicidade. N&atilde;o guardo nada relacionado ao que poderia ter sido ou arrependimentos, lembro do que tivemos ali e isso j&aacute; &eacute; marcante mais que o suficiente, pelo fato de ter sido com voc&ecirc; e pensar que voc&ecirc;, talvez, sinta o mesmo, me alegra.</p>
+
+<p>No cen&aacute;rio em que n&atilde;o paramos, meu vocabul&aacute;rio, infelizmente, n&atilde;o &eacute; vasto suficiente pra transformar em palavras o que n&oacute;s dois somos quando estamos jutnos e pode ser melhor assim, porque temos uma rela&ccedil;&atilde;o, literalmente, indescrit&iacute;vel, e sempre seremos os &uacute;nicos que v&atilde;o conseguir entender por completo o que ela significa.<br />
+<br />
+N&atilde;o quero que voc&ecirc; indague se as &uacute;nicas passagens que me d&atilde;o vontade de escrever s&atilde;o s&oacute; essas horas, com essa tens&atilde;o espec&iacute;fica. Cada uma das hist&oacute;rias que ouvimos, falamos, vivemos e dividimos desde que come&ccedil;amos essa nova fase at&eacute; agora, tamb&eacute;m foram extremamente importantes pra mim. Nada teria chegado nesse n&iacute;vel sem essa base fundamental de seguran&ccedil;a que sinto com voc&ecirc;. O que eu me tornei, ou pude descobrir que sempre fui, s&oacute; foi poss&iacute;vel gra&ccedil;as a voc&ecirc; que n&atilde;o s&oacute; me aceitou com alguns problemas, como me salvou do pior deles. E por isso, eu sou grato e pe&ccedil;o por favor, para que n&atilde;o assuma que nada que eu fiz ou farei s&atilde;o, de alguma forma, a&ccedil;&otilde;es de retribui&ccedil;&atilde;o, porque n&atilde;o foram e n&atilde;o ser&atilde;o. Um sentimento n&atilde;o interfere no outro, eles coexistem paralelamente.<br />
+<br />
+Quando voc&ecirc; sai, tudo isso que era tang&iacute;vel com abra&ccedil;os, beijos, com aquele sorriso, se desmontam e voltam ao estado de emo&ccedil;&otilde;es que eu me esfor&ccedil;o pra pelo menos traduzir em palavras escritas. Acho que a melhor frase pra descrever o porqu&ecirc; isso ocorre &eacute;, tamb&eacute;m, uma das suas favoritas:<br />
+<br />
+<span style="text-align:center;">&quot;Se eu te amasse menos, talvez fosse capaz de falar mais sobre o assunto.&quot; </span></p>
+
+<hr /></div>
+</body>
+</html>
