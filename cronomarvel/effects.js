5fb27a779cee207fc1ec6e2e0cc888e06a21b74b
Hudson Souza <hudson.souza@symphony.com>
2021-03-17 22:54:18 -0300

    added time left

--- cronomarvel/effects.js
+++ cronomarvel/effects.js
@@ -1,5 +1,33 @@
 function Watched() {
 
+	tempo = localStorage.getItem('tempo');
+	if (tempo == null) {
+		localStorage.setItem('tempo', 2884);
+	} else {
+		Converter(tempo);
+	}
+
+	function Converter(tempoRestante) {
+		console.log("func", tempoRestante)
+		hours = Math.floor(tempoRestante / 60);
+		minutes = tempoRestante % 60;
+		tempoRestante = hours + " horas e " + minutes + " minutos";
+		document.getElementById('tempo').innerHTML = tempoRestante;
+	}
+
+	function Tempo(i, watched) {
+		tempoFilme = document.getElementById(i).getAttribute('data-time');
+		tempoRestante = localStorage.getItem('tempo');
+		if (watched == true) {
+			tempoRestante = tempoRestante - tempoFilme;
+		} else {
+			tempoRestante = +tempoRestante + +tempoFilme;
+		}
+		console.log(tempoFilme);
+		Converter(tempoRestante);
+		localStorage.setItem('tempo', tempoRestante);
+	}
+	
 	qtdFilmes = document.getElementsByClassName('movie').length;
 	for (var i = 1; i <= qtdFilmes; i++) {
 		valida = 'filme'+i;
@@ -15,6 +43,15 @@ function Watched() {
 	    item = 'filme'+this.id;
 	    status = this.className;
 	    localStorage.setItem(item, status);
+	    i = this.id;
+	    if (this.classList == "movie watched") {
+	    	watched = true;
+	    	Tempo(i, watched);
+	    }
+	    else {
+	    	watched = false;
+	    	Tempo(i, watched);
+	    }
 	  });
 	});
 }
