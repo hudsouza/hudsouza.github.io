29f7b0eb6f69712ff2896c1b51a1901ef79049fb
Hudson Souza <hudsonmacedo14@gmail.com>
2023-01-18 19:27:41 -0300

    Create createRank.js

---
+++ top5/createRank.js
@@ -0,0 +1,82 @@
+function selectItem() {
+	const item = [
+		"Brigadeiro",
+		"Coxinha",
+		"Lasanha",
+		"Macarrão",
+		"Churrasco",
+		"Pastel",
+		"Hamburger",
+		"Salada",
+		"Pão",
+		"Sanduíche",
+		"Bolo",
+		"Torta doce",
+		"Torta salgada",
+		"Arroz",
+		"Feijão",
+		"Pizza",
+		"Cachorro quente",
+		"Purê de batata",
+		"Batata frita",
+		"Fígado",
+		"Frango",
+		"Farofa",
+		"Queijo",
+		"Sorvete",
+		"Beijinho",
+		"Paçoca",
+		"Feijoada",
+		"Brócolis",
+		"Pamonha",
+		"Kibe",
+		"Bolinha de queijo",
+		"Goiabada",
+		"Pudim",
+		"Biscoito de polvilho",
+		"Bolacha recheada",
+		"Pão de queijo",
+		"Sushi",
+		"Pavê",
+		"Sonho",
+		"Panqueca salgada",
+		"Chocolate",
+		"Abóbora",
+		"Abobrinha",
+		"Beterraba",
+		"Berinjela",
+		"Mousse",
+		"Jiló",
+		"Quiabo",
+		"Canjica",
+		"Sopa",
+		"Fruta",
+	]
+	let i = Math.floor(item.length*Math.random());
+	document.getElementById('item').innerHTML = item[i];
+};
+
+var count = 0;
+
+function pickOrder(top){
+	const picked =  document.getElementById('item').innerHTML;
+	document.getElementById('top'+top).innerHTML = picked;
+	document.getElementById('btn'+top).style.display = 'none';
+	if (count == 0) {
+		count = 1;
+		selectItem();
+	} else if (count == 1) {
+		count = 2;
+		selectItem();
+	} else if (count == 2) {
+		count = 3;
+		selectItem();
+	} else if (count == 3) {
+		count = 4;
+		selectItem();
+	}else if (count == 4) {
+		count = 0;
+		document.getElementById('startHead').innerHTML = "Seu Top 5 é:";
+		document.getElementById('startTitle').innerHTML = 'Parabéns!';
+	}
+};
